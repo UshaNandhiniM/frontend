2a7f250e1ceeb4e701ffbd83b3b7e13663ceb402
UshaNandhiniM <ushanandhinim123@gmail.com>
2024-08-01 06:53:26 +0530

    updated

--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -1,47 +1,47 @@
-import { Button } from 'flowbite-react';
-import React from 'react';
-import {  AiFillGoogleSquare } from 'react-icons/ai';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router';
-import {app} from '../firebase';
-import { signInFailure, signInSuccess } from '../Redux/AuthSlice';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
-
+import { Button } from "flowbite-react";
+import React from "react";
+import { AiFillGoogleSquare } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { app } from "../firebase";
+import { signInFailure, signInSuccess } from "../Redux/AuthSlice";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const OAuth = () => {
-    const auth= getAuth(app)
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const handlesubmit=async()=>{
-        const provider = new GoogleAuthProvider();
-        provider.setCustomParameters({prompt:'select_account'})
-        try {
-            const result = await signInWithPopup(auth, provider);
-            const res = await fetch("https://car-service-backend-5142.onrender.com/api/user/google",{
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({ 
-                    name: result.user.displayName,
-                    email: result.user.email,
-                    profilePic: result.user.photoURL,})
-            })
-            const data = await res.json()
-            if(res.ok)
-                localStorage.setItem("Token",data.token)
-            dispatch(signInSuccess(data))
-            navigate('/')
-            
-        } catch (error) {
-            dispatch(signInFailure(error))
-            
+  const auth = getAuth(app);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handlesubmit = async () => {
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const res = await fetch(
+        "https://car-service-backend-5142.onrender.com/api/user/google",
+        {
+          method: "POST",
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            name: result.user.displayName,
+            email: result.user.email,
+            profilePic: result.user.photoURL,
+          }),
         }
+      );
+      const data = await res.json();
+      if (res.ok) localStorage.setItem("Token", data.token);
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error));
     }
-    return (
-       <Button type='button' >
-        <AiFillGoogleSquare className='w-6 h-6 mr-2' onClick={handlesubmit}/>
-        Continue with Google
-       </Button>
-    );
+  };
+  return (
+    <Button type="button">
+      <AiFillGoogleSquare className="w-6 h-6 mr-2" onClick={handlesubmit} />
+      Continue with Google
+    </Button>
+  );
 };
 
 export default OAuth;
